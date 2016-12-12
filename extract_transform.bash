596f33365902be33806de4e96e97db50386ae839
David Maier <david@Davids-MacBook.fritz.box>
2016-12-12 15:23:24 +0100

    Extract and transform persons

--- extract_transform.bash
+++ extract_transform.bash
@@ -1,2 +1,2 @@
 # Extract all Persons
-cat movies.neo4j | grep Person | cut -f2 -d'(' | cut -f1 -d')' | sed 's/Person/Person" :/g' | awk '{print "\""$0}'
+cat movies.neo4j | grep Person | cut -f2 -d'(' | cut -f1 -d')' | sed 's/Person/Person" :/g' | awk '{print "\""$0}' | sed 's/{[a-z]*:/"&"/g' | sed 's/:"/" : /g' | sed 's/"{/{"/g' | sed 's/, [a-z]*:/"&"/g' | sed 's/:"/":/g' | sed 's/", / ,"/g' | sed "s/\'/\"/g"
